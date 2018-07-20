56de925e5681b07b553cf1e739a37acdd68b094c
Kyle Workman <kyleworkman15@augustana.edu>
2018-07-20 09:23:09 -0500

    Transition to new firebase project

--- public/app.js
+++ public/app.js
@@ -23,7 +23,7 @@ function googleLogin() {
 // Check that the user is a dispatcher.
 function afterLogin(user) {
 	const email = user.email;
-	if (email === "kyleworkman15@augustana.edu" || email === "csc490teama@augustana.edu") {
+	if (email === "kyleworkman15@augustana.edu" || email === "csc490teama@augustana.edu" || email === "acesdispatcher@augustana.edu") {
 		correctLogin();
 	} else {
 		alert("Sorry, you must be a dispatcher.");
